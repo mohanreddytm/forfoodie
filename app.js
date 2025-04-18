99d57c6b0f858527957d6e09fe90db97220ca741
mohanreddytm <tamminenimohanreddy1@gmail.com>
2025-04-18 10:44:46 +0530

    initialization completed need updation based on the requiremnt of frontend

--- app.js
+++ app.js
@@ -39,73 +39,73 @@ app.get("/products/" , async (request, response) => {
   }
 });
 
-// app.post("/users/", async (request, response) => {
-//   const { name, contact, email, password } = request.body;
+app.post("/users/", async (request, response) => {
+  const { name, contact, email, password } = request.body;
 
-//   console.log(name, contact, email, password);
+  console.log(name, contact, email, password);
   
-//   try {
-//     // Check if user exists
-//     const userCheck = await pool.query(
-//       'SELECT * FROM "User" WHERE email = $1;',
-//       [email]
-//     );
+  try {
+    // Check if user exists
+    const userCheck = await pool.query(
+      'SELECT * FROM "User" WHERE email = $1;',
+      [email]
+    );
 
-//     if (userCheck.rows.length > 0) {
-//       return response.status(400).send("User already exists");
-//     }
+    if (userCheck.rows.length > 0) {
+      return response.status(400).send("User already exists");
+    }
 
-//     // Create new user
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     await pool.query(
-//       'INSERT INTO "User" (name,contact, email, password) VALUES ($1, $2, $3, $4);',
-//       [name,contact, email, hashedPassword]
-//     );
+    // Create new user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await pool.query(
+      'INSERT INTO "User" (name,contact, email, password) VALUES ($1, $2, $3, $4);',
+      [name,contact, email, hashedPassword]
+    );
 
-//     const result = await pool.query('SELECT * FROM public."User" WHERE email = $1;', [email]);
-//     const usersId = (result.rows[0].id);
+    const result = await pool.query('SELECT * FROM public."User" WHERE email = $1;', [email]);
+    const usersId = (result.rows[0].id);
 
 
-//     const payload = {username: email, userId: usersId};
+    const payload = {username: email, userId: usersId};
     
-//     const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN", { expiresIn: "30d" });
-//     response.json({ jwtToken });
-//   } catch (error) {
-//     console.error("POST Error:", error);
-//     response.status(500).send("Registration failed");
-//   }
-// });
+    const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN", { expiresIn: "30d" });
+    response.json({ jwtToken });
+  } catch (error) {
+    console.error("POST Error:", error);
+    response.status(500).send("Registration failed");
+  }
+});
 
-// // User login
-// app.post("/login/", async (request, response) => {
-//   const { email, password } = request.body;
+// User login
+app.post("/login/", async (request, response) => {
+  const { email, password } = request.body;
   
-//   try {
-//     const result = await pool.query(
-//       'SELECT * FROM "User" WHERE email = $1;',
-//       [email]
-//     );
+  try {
+    const result = await pool.query(
+      'SELECT * FROM "User" WHERE email = $1;',
+      [email]
+    );
     
-//     if (result.rows.length === 0) {
-//       return response.status(400).send("Invalid user");
-//     }
+    if (result.rows.length === 0) {
+      return response.status(400).send("Invalid user");
+    }
 
-//     const dbUser = result.rows[0];
-//     const isPasswordValid = await bcrypt.compare(password, dbUser.password);
+    const dbUser = result.rows[0];
+    const isPasswordValid = await bcrypt.compare(password, dbUser.password);
     
-//     if (!isPasswordValid) {
-//       return response.status(400).send("Invalid password");
-//     }
+    if (!isPasswordValid) {
+      return response.status(400).send("Invalid password");
+    }
 
-//     const payload = {username: dbUser.email,userId: dbUser.id,};
-//     const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN", { expiresIn: "30d" });
-//     response.json({ jwtToken });
+    const payload = {username: dbUser.email,userId: dbUser.id,};
+    const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN", { expiresIn: "30d" });
+    response.json({ jwtToken });
     
-//   } catch (error) {
-//     console.error("Login Error:", error);
-//     response.status(500).send("Login failed");
-//   }
-// });
+  } catch (error) {
+    console.error("Login Error:", error);
+    response.status(500).send("Login failed");
+  }
+});
 
 
 app.listen(PORT, () => {
